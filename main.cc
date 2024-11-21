d388f442dd9deb2a19e4aa5a74d0969bb7a86250
OverbearingPearl <OverbearingPearl@outlook.com>
2024-11-21 15:19:46 +0800

    feat: add draft of memoization

---
+++ main.cc
@@ -0,0 +1,45 @@
+/*
+ Copyright (c) 2024 OverbearingPearl
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+#include <iostream>
+
+#include "src/side_effects/memoization/memoization.h"
+
+int main() {
+  side_effects::memoization::Memoization memoization;
+  auto fib = memoization.memoize([](int n) -> int {
+    if (n < 2) {
+      return n;
+    }
+    int a = 0, b = 1, c;
+    for (int i = 2; i <= n; ++i) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+  });
+  int result = fib(10);
+  std::cout << "Fibonacci of 10 is " << result << std::endl;
+
+  return 0;
+}
---
+++ src/side_effects/memoization/memoization.h
@@ -0,0 +1,117 @@
+/*
+ Copyright (c) 2024 OverbearingPearl
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+#ifndef SRC_SIDE_EFFECTS_MEMOIZATION_MEMOIZATION_H_
+#define SRC_SIDE_EFFECTS_MEMOIZATION_MEMOIZATION_H_
+
+#include <functional>
+#include <memory>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+
+namespace side_effects {
+namespace memoization {
+
+struct TupleHash {
+  template <typename... Args>
+  std::size_t operator()(const std::tuple<Args...>& t) const {
+    return hashTuple(t);
+  }
+
+ private:
+  template <typename Tuple,
+            std::size_t Index = std::tuple_size<Tuple>::value - 1>
+  struct Hasher {
+    std::size_t operator()(const Tuple& t) const {
+      std::size_t hash = Hasher<Tuple, Index - 1>()(t);
+      TupleHash::hashCombine(hash, std::get<Index>(t));
+      return hash;
+    }
+  };
+
+  template <typename Tuple>
+  struct Hasher<Tuple, 0> {
+    std::size_t operator()(const Tuple& t) const {
+      std::size_t hash = 0;
+      TupleHash::hashCombine(hash, std::get<0>(t));
+      return hash;
+    }
+  };
+
+  template <typename Tuple>
+  std::size_t hashTuple(const Tuple& t) const {
+    return Hasher<Tuple>()(t);
+  }
+
+  template <typename T>
+  static void hashCombine(std::size_t& seed, const T& value) {
+    std::hash<T> hasher;
+    seed ^= hasher(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+  }
+};
+
+struct TupleEqual {
+  template <typename... Args>
+  bool operator()(const std::tuple<Args...>& t1,
+                  const std::tuple<Args...>& t2) const {
+    return t1 == t2;
+  }
+};
+
+class Memoization {
+ public:
+  template <typename Func>
+  auto memoize(Func func)
+      -> std::function<decltype(func(std::declval<int>()))(int)> {
+    return [this, func](int arg) -> decltype(func(arg)) {
+      using KeyType = std::tuple<int>;
+      using ResultType = decltype(func(arg));
+
+      KeyType key = std::make_tuple(arg);
+      auto& cache = getCache<KeyType, ResultType>();
+      auto it = cache.find(key);
+      if (it == cache.end()) {
+        ResultType result = func(arg);
+        cache[key] = std::make_shared<ResultType>(result);
+        return result;
+      }
+      return *std::static_pointer_cast<ResultType>(it->second);
+    };
+  }
+
+ private:
+  template <typename KeyType, typename ResultType>
+  using CacheType = std::unordered_map<KeyType, std::shared_ptr<ResultType>,
+                                       TupleHash, TupleEqual>;
+
+  template <typename KeyType, typename ResultType>
+  CacheType<KeyType, ResultType>& getCache() const {
+    static CacheType<KeyType, ResultType> cache;
+    return cache;
+  }
+};
+
+}  // namespace memoization
+}  // namespace side_effects
+
+#endif  // SRC_SIDE_EFFECTS_MEMOIZATION_MEMOIZATION_H_
